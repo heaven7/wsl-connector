520c487de18e579237e63fa2727613f14433198c
Christian Grlica <christian@heavenseven.net>
2015-11-07 15:57:24 +0100

    update packages, meteor v1.2

--- package.js
+++ package.js
@@ -1,19 +1,15 @@
 Package.describe({
   name: 'heaven7:wsl-connector',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
+  version: '0.0.2',
   summary: 'Package-Integration for wsl-* packages.',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/heaven7/wsl-connector.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 both = ['client','server'];
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.2');
 
   api.addFiles([
       'lib/both/wsl-locations/register.js',
